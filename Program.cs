7825ecf0b661d5c4d24c889fa2f9bed38faa94d4
Muskan <muskansikka1234@gmail.com>
2025-08-11 17:32:40 +0530

    Added calculator source code

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Simple Calculator ====");
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose an operation: (+, -, *, /)");
+            char op = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            double result = 0;
+            bool validOperation = true;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                        validOperation = false;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation selected.");
+                    validOperation = false;
+                    break;
+            }
+
+            if (validOperation)
+            {
+                Console.WriteLine($"Result: {num1} {op} {num2} = {result}");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
